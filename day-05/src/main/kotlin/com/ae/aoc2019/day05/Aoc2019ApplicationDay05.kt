a8fb701c7d73e853bb30b390cb8016ca4e18e01f
AE <ae@ae>
2019-12-05 21:15:14 +0100

    Sollution to part 01 of Day 05

--- day-05/src/main/kotlin/com/ae/aoc2019/day05/Aoc2019ApplicationDay05.kt
+++ day-05/src/main/kotlin/com/ae/aoc2019/day05/Aoc2019ApplicationDay05.kt
@@ -13,14 +13,11 @@ import java.lang.RuntimeException
 @SpringBootApplication
 class Aoc2019ApplicationDay05 : CommandLineRunner {
 
-	private val logger = LoggerFactory.getLogger(javaClass)
-
-	private fun getValue(instruction: String, parameter: Int, pc: Int, program: ArrayList<Int>) : Int {
-		return if (instruction[2 - parameter].toString().toInt() == 1)
-					program[pc + parameter + 1]
-		       else
-					program[program[pc + parameter  + 1]]
-	}
+	private fun getValue(instruction: String, parameter: Int, pc: Int, program: ArrayList<Int>) : Int =
+		if (instruction[2 - parameter].toString().toInt() == 1)
+			program[pc + parameter + 1]
+	    else
+			program[program[pc + parameter  + 1]]
 
 	private fun runProgram(program: ArrayList<Int>,
 						   inputs: Iterator<Int>) : Int {
@@ -29,7 +26,6 @@ class Aoc2019ApplicationDay05 : CommandLineRunner {
 		while (true) {
 			val instruction = program[pc].toString().padStart(5, '0')
 			val opcode = instruction.substring(3).toInt()
-			var inc: Int
 			if (opcode == 99) {
 				break
 			} else if (opcode == 1) {
@@ -50,42 +46,27 @@ class Aoc2019ApplicationDay05 : CommandLineRunner {
 				pc += 2
 			} else if (opcode == 4) {
 				val v1 = getValue(instruction, 0, pc, program)
-				System.out.println("Output $v1")
+				System.out.println(v1)
 				pc += 2
 			} else if (opcode == 5) {
-				/* Opcode 5 is jump-if-true:
-				if the first parameter is non-zero,
-				it sets the instruction pointer to the value from the second parameter.
-				Otherwise, it does nothing. */
 				val v1 = getValue(instruction, 0, pc, program)
 				if (v1 != 0)
 					pc = getValue(instruction, 1, pc, program)
 			    else
 					pc += 3
 			} else if (opcode == 6) {
-				/*Opcode 6 is jump-if-false:
-				if the first parameter is zero,
-				it sets the instruction pointer to the value from the second parameter.
-				Otherwise, it does nothing*/
 				val v1 = getValue(instruction, 0, pc, program)
 				if (v1 == 0)
 					pc = getValue(instruction, 1, pc, program)
 				else
 					pc += 3
 			} else if (opcode == 7) {
-				/*Opcode 7 is less than:
-				   if the first parameter is less than the second parameter,
-				   it stores 1 in the position given by the third parameter.
-				   Otherwise, it stores 0*/
 				val v1 = getValue(instruction, 0, pc, program)
 				val v2 = getValue(instruction, 1, pc, program)
 				val dest = program[pc + 3]
 				program[dest] = if (v1 < v2) 1 else 0
 				pc += 4
 			} else if (opcode == 8) {
-				/*Opcode 8 is equals: if the first parameter is equal to the second parameter,
-				it stores 1 in the position given by the third parameter.
-				Otherwise, it stores 0.*/
 				val v1 = getValue(instruction, 0, pc, program)
 				val v2 = getValue(instruction, 1, pc, program)
 				val dest = program[pc + 3]
@@ -95,7 +76,6 @@ class Aoc2019ApplicationDay05 : CommandLineRunner {
 				throw RuntimeException("Illegal Instruction ${opcode}")
 			}
 		}
-
 		return program[0]
 	}
 
@@ -104,11 +84,8 @@ class Aoc2019ApplicationDay05 : CommandLineRunner {
 		val bufferedReader = BufferedReader(InputStreamReader(Aoc2019ApplicationDay05::class.java.getResourceAsStream(args[0]!!)))
 		bufferedReader.useLines {
 			it.forEach { line ->
-				val program = ArrayList(line.split(",").map { it.toInt() })
-				System.out.println("Airco Stuff")
-				runProgram(program, arrayOf(1).iterator())
-				System.out.println("Thermal Stuff")
-				runProgram(program, arrayOf(5).iterator())
+				runProgram(ArrayList(line.split(",").map { it.toInt() }), arrayOf(1).iterator())
+				runProgram(ArrayList(line.split(",").map { it.toInt() }), arrayOf(5).iterator())
 			}
 		}
 	}
