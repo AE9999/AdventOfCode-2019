7f0630511d73910e30aa0689bb54a4670ca61b23
ae <ae>
2019-12-06 10:59:46 +0100

    Adds the sollution for Day 06 part 2

--- day-06/src/main/kotlin/com/ae/aoc2019/day06/Aoc2019ApplicationDay06.kt
+++ day-06/src/main/kotlin/com/ae/aoc2019/day06/Aoc2019ApplicationDay06.kt
@@ -1,14 +1,11 @@
 package com.ae.aoc2019.day06
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import java.io.BufferedReader
-import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.Exception
-import java.lang.RuntimeException
+import java.lang.IllegalStateException
 import java.util.*
 
 @SpringBootApplication
@@ -30,41 +27,52 @@ class Aoc2019ApplicationDay06 : CommandLineRunner {
 		return nOrbits
 	}
 
-
-
     private fun findNrOfOrbits(source: String,
                                targets: Set<String>,
                                fp: HashMap<String, ArrayList<String>>) : Int {
-        var optimalPath = Int.MAX_VALUE
+
         val seen = HashSet<String>()
+
         val toDo = Stack<Pair<String, Int>>()
+        toDo.push(Pair(source, 0))
         while (!toDo.isEmpty()) {
             val currentId = toDo.peek().first
             val currentDistance = toDo.peek().second
             toDo.pop()
             val orbits = fp.getOrDefault(currentId, ArrayList())
-            orbits.filter { !seen.contains(it) }.forEach { toDo.push(it) }
+            seen.add(currentId)
+            orbits.filter { !seen.contains(it) }
+                  .forEach {
+                      if (targets.contains(it))
+                          return currentDistance
+                      else
+                          toDo.push(Pair(it, currentDistance + 1))
+                  }
         }
-        return optimalPath
+        throw IllegalStateException("Graph was assumed to be connected ..")
     }
 
 	override fun run(vararg args: String?) {
         val nodes = HashSet<String>()
-        val fp = HashMap<String, ArrayList<String>>()
+        val orbitsDirected = HashMap<String, ArrayList<String>>()
+        val orbitsUnDirected = HashMap<String, ArrayList<String>>()
 		val bufferedReader = BufferedReader(InputStreamReader(Aoc2019ApplicationDay06::class.java.getResourceAsStream(args[0]!!)))
 		bufferedReader.useLines {
 			it.forEach { line ->
 				line.split(')').forEach { nodes.add(it) }
 				val pair = line.split(')').zipWithNext().map { Pair(it.second, it.first) }.first()
-				if (!fp.containsKey(pair.first)) {
-					fp[pair.first] = ArrayList()
-				}
-				fp[pair.first]!!.add(pair.second)
+				if (!orbitsDirected.containsKey(pair.first)) {  orbitsDirected[pair.first] = ArrayList() }
+                if (!orbitsUnDirected.containsKey(pair.first)) {  orbitsUnDirected[pair.first] = ArrayList()  }
+                if (!orbitsUnDirected.containsKey(pair.second)) { orbitsUnDirected[pair.second] = ArrayList() }
+				orbitsDirected[pair.first]!!.add(pair.second)
+                orbitsUnDirected[pair.first]!!.add(pair.second)
+                orbitsUnDirected[pair.second]!!.add(pair.first)
 			}
 		}
-		System.out.println("Found ${nodes.map { countParents(it, fp) }.sum()} direct and indirect orbits")
+		System.out.println("Found ${nodes.map { countParents(it, orbitsDirected) }.sum()} direct and indirect orbits")
 
-        val targets =  fp["SAN"]!!
+        val targets =  orbitsDirected["SAN"]!!.toSet()
+        System.out.println("Minimum number of orbital transfers required: ${findNrOfOrbits("YOU", targets, orbitsUnDirected)}")
 
 	}
 }
