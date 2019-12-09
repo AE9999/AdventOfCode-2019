c86bff19a0d458d9c768b72fa8c0fd46cb7c63d1
ae <ae>
2019-12-09 08:38:08 +0100

    Adds a BigIntegerSollution for Day09 part 01 & part 02.

--- day-09/src/main/kotlin/com/ae/aoc2019/day09/Aoc2019ApplicationDay09.kt
+++ day-09/src/main/kotlin/com/ae/aoc2019/day09/Aoc2019ApplicationDay09.kt
@@ -5,78 +5,98 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.lang.IllegalStateException
 import java.lang.RuntimeException
+import java.math.BigInteger
 import java.util.*
-import kotlin.collections.ArrayList
 
 @SpringBootApplication
-class Aoc2019ApplicationDay09() : CommandLineRunner {
+class Aoc2019ApplicationDay09 : CommandLineRunner {
 
-	class Program(val program: MutableList<Int>) {
-		var pc = 0
+	class Program(val programMem: MutableMap<BigInteger, BigInteger>) {
+		var pc : BigInteger = BigInteger.ZERO
+		var relativeBase: BigInteger = BigInteger.ZERO
 
-		private fun getValue(instruction: String, parameter: Int, pc: Int, program: MutableList<Int>) : Int =
-				if (instruction[2 - parameter].toString().toInt() == 1)
-					program[pc + parameter + 1]
-				else
-					program[program[pc + parameter  + 1]]
+		private fun getDestination(instruction: String, parameter: Int) : BigInteger {
+			if (instruction[2 - parameter].toString().toInt() == 2) {
+				return relativeBase + programMem.getValue(pc + parameter.toBigInteger() + 1.toBigInteger())
+			} else if (instruction[2 - parameter].toString().toInt() == 1) {
+				return pc + parameter.toBigInteger() + BigInteger.ONE
+			} else {
+				return programMem.getValue(pc + parameter.toBigInteger() +  1.toBigInteger())
+			}
+		}
 
-		fun run(inputs: Iterator<Int>) : Int {
+		private fun readMem(instruction: String, parameter: Int) : BigInteger {
+			if (instruction[2 - parameter].toString().toInt() == 2) {
+				val argumentValue = programMem.getValue(pc + parameter.toBigInteger() + BigInteger.ONE)
+				return programMem.getValue(relativeBase + argumentValue) // Not plus one?
+			} else if (instruction[2 - parameter].toString().toInt() == 1) {
+				val index = pc + parameter.toBigInteger() + BigInteger.ONE
+				return programMem.getValue(index)
+			} else {
+				val index = programMem.getValue(pc + parameter.toBigInteger() + BigInteger.ONE)
+				return  programMem.getValue(index)
+			}
+		}
+
+		fun run(inputs: Iterator<BigInteger>)  {
 			while (true) {
-				val instruction = program[pc].toString().padStart(5, '0')
+				val instruction = programMem[pc].toString().padStart(5, '0')
 				val opcode = instruction.substring(3).toInt()
 				if (opcode == 99) {
-					return -1
+					break
 				} else if (opcode == 1) {
-					val v1 =  getValue(instruction, 0, pc, program)
-					val v2 = getValue(instruction, 1, pc, program)
-					val dest = program[pc + 3]
-					program[dest] = v1 + v2
-					pc += 4
+					val v1 =  readMem(instruction, 0)
+					val v2 = readMem(instruction, 1)
+					val dest = getDestination(instruction, 2)
+					programMem[dest] = v1 + v2
+					pc += 4.toBigInteger()
 				} else if (opcode == 2) {
-					val v1 = getValue(instruction, 0, pc, program)
-					val v2 = getValue(instruction, 1, pc, program)
-					val dest = program[pc + 3]
-					program[dest] = v1 * v2
-					pc += 4
+					val v1 = readMem(instruction, 0)
+					val v2 = readMem(instruction, 1)
+					val dest = getDestination(instruction, 2)
+					programMem[dest] = v1 * v2
+					pc += 4.toBigInteger()
 				} else if (opcode == 3) {
-					val v1 = program[pc + 1]
-					program[v1] = inputs.next()
-					pc += 2
+					val v1 = getDestination(instruction, 0)
+					programMem[v1] = inputs.next()
+					pc += 2.toBigInteger()
 				} else if (opcode == 4) {
-					val v1 = getValue(instruction, 0, pc, program)
-					pc += 2
-					return v1
+					val v1 = readMem(instruction, 0)
+					pc += 2.toBigInteger()
+					println(v1)
 				} else if (opcode == 5) {
-					val v1 = getValue(instruction, 0, pc, program)
+					val v1 = readMem(instruction, 0).toInt()
 					if (v1 != 0)
-						pc = getValue(instruction, 1, pc, program)
+						pc = readMem(instruction, 1)
 					else
-						pc += 3
+						pc += 3.toBigInteger()
 				} else if (opcode == 6) {
-					val v1 = getValue(instruction, 0, pc, program)
+					val v1 = readMem(instruction, 0).toInt()
 					if (v1 == 0)
-						pc = getValue(instruction, 1, pc, program)
+						pc = readMem(instruction, 1)
 					else
-						pc += 3
+						pc += 3.toBigInteger()
 				} else if (opcode == 7) {
-					val v1 = getValue(instruction, 0, pc, program)
-					val v2 = getValue(instruction, 1, pc, program)
-					val dest = program[pc + 3]
-					program[dest] = if (v1 < v2) 1 else 0
-					pc += 4
+					val v1 = readMem(instruction, 0)
+					val v2 = readMem(instruction, 1)
+					val dest = getDestination(instruction, 2)
+					programMem[dest] = if (v1 < v2) BigInteger.ONE else BigInteger.ZERO
+					pc += 4.toBigInteger()
 				} else if (opcode == 8) {
-					val v1 = getValue(instruction, 0, pc, program)
-					val v2 = getValue(instruction, 1, pc, program)
-					val dest = program[pc + 3]
-					program[dest] = if (v1 == v2) 1 else 0
-					pc += 4
+					val v1 = readMem(instruction, 0)
+					val v2 = readMem(instruction, 1)
+					val dest = getDestination(instruction, 2)
+					programMem[dest] = if (v1 == v2) BigInteger.ONE else BigInteger.ZERO
+					pc += 4.toBigInteger()
+				} else if (opcode == 9) {
+					val v1 = readMem(instruction, 0)
+					relativeBase += v1
+					pc += 2.toBigInteger()
 				} else {
 					throw RuntimeException("Illegal Instruction ${opcode}")
 				}
 			}
-			throw RuntimeException("Program terminated without output ..")
 		}
 	}
 
@@ -84,7 +104,16 @@ class Aoc2019ApplicationDay09() : CommandLineRunner {
 		val bufferedReader = BufferedReader(InputStreamReader(Aoc2019ApplicationDay09::class.java.getResourceAsStream(args[0]!!)))
 		bufferedReader.useLines {
 			it.forEach { line ->
-
+				val rawProgram = line.split(',')
+				val programMem = HashMap<BigInteger, BigInteger>().withDefault { BigInteger.ZERO }
+				for (i in 0 until rawProgram.size) {
+					programMem[i.toBigInteger()] = BigInteger(rawProgram[i])
+				}
+				var program : Program
+				program = Program(programMem.toMutableMap())
+				program.run(arrayOf(BigInteger("1")).iterator())
+				program = Program(programMem.toMutableMap())
+				program.run(arrayOf(BigInteger("2")).iterator())
 			}
 		}
 	}
