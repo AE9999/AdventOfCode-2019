946ec5cd298604c034fdc2ef536d2683707bbe5f
ae <ae>
2019-12-16 06:33:21 +0100

    Adds a day 16 scaffold

---
+++ day-16/src/main/kotlin/com/ae/aoc2019/day16/Aoc2019ApplicationDay16.kt
@@ -0,0 +1,22 @@
+package com.ae.aoc2019.day16
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.RuntimeException
+import java.math.BigInteger
+import kotlin.collections.HashMap
+
+@SpringBootApplication
+class Aoc2019ApplicationDay16 : CommandLineRunner {
+
+	override fun run(vararg args: String?) {
+		val bufferedReader = BufferedReader(InputStreamReader(Aoc2019ApplicationDay16::class.java.getResourceAsStream(args[0]!!)))
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<Aoc2019ApplicationDay16>(*args)
+}
