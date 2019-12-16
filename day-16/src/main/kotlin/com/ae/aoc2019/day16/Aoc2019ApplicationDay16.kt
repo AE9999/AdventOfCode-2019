505bf895fa72ab98e88aa5b01659b8a9434e3834
ae <ae>
2019-12-16 08:42:11 +0100

    Adds a preliminary sollution for Day 16

--- day-16/src/main/kotlin/com/ae/aoc2019/day16/Aoc2019ApplicationDay16.kt
+++ day-16/src/main/kotlin/com/ae/aoc2019/day16/Aoc2019ApplicationDay16.kt
@@ -12,8 +12,46 @@ import kotlin.collections.HashMap
 @SpringBootApplication
 class Aoc2019ApplicationDay16 : CommandLineRunner {
 
+	fun calculatedatter(pattern: List<Int>, position: Int) : List<Int> {
+		return pattern.flatMap { element ->
+			                 (0 until position)
+				          .map { element } }
+				      .drop(1)
+	}
+
+	fun applyPatternToDigit(subSignal : List<Int>, pattern: List<Int>) : Int {
+		var rvalue = 0
+		for (i in 0 until subSignal.size) {
+			rvalue +=  subSignal[i] * pattern[i % pattern.size]
+		}
+		return rvalue.toString().toCharArray().last().toString().toInt()
+	}
+
+	fun applyPattern(signal: List<Int>, pattern: List<Int>) : List<Int> {
+		var rvalue = ArrayList<Int>()
+		for (i in 0 until signal.size) {
+			val calculatedPatter = calculatedatter(pattern, i)
+			rvalue.add( applyPatternToDigit(signal.drop(i+1), calculatedPatter) )
+		}
+		return rvalue
+	}
+
 	override fun run(vararg args: String?) {
 		val bufferedReader = BufferedReader(InputStreamReader(Aoc2019ApplicationDay16::class.java.getResourceAsStream(args[0]!!)))
+
+
+		bufferedReader.useLines {
+			it.forEach { line ->
+				val basePattern = listOf(0, 1, 0, -1)
+				var numbers = line.toCharArray().map { it.toString().toInt() }
+				(0 until 100).forEach {
+					val pattern = ArrayList<Int>()
+					numbers = applyPattern(numbers, pattern)
+					println("Result: $numbers")
+				}
+
+			}
+		}
 	}
 }
 
