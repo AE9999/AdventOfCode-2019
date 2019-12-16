08efca827815094c8fb95bd0aab92c3f9cf12162
ae <ae>
2019-12-16 14:33:22 +0100

    Adds a really slow / stupid sollution for part 2

--- day-16/src/main/kotlin/com/ae/aoc2019/day16/Aoc2019ApplicationDay16.kt
+++ day-16/src/main/kotlin/com/ae/aoc2019/day16/Aoc2019ApplicationDay16.kt
@@ -12,10 +12,9 @@ import kotlin.collections.HashMap
 @SpringBootApplication
 class Aoc2019ApplicationDay16 : CommandLineRunner {
 
+	fun getIterator(position : Long, pattern: List<Long>) : Iterator<Long> {
 
-	fun getIterator(position : Int, pattern: List<Int>) : Iterator<Int> {
-
-		return object : Iterator<Int> {
+		return object : Iterator<Long> {
 
 			var currentIterator = pattern.map { element ->
 												(0..position)
@@ -28,7 +27,7 @@ class Aoc2019ApplicationDay16 : CommandLineRunner {
 				return true
 			}
 
-			override fun next(): Int {
+			override fun next(): Long {
 				if (!currentIterator.hasNext()) {
 					currentIterator = pattern.map { element ->
 													(0..position).map { element } }
@@ -40,16 +39,17 @@ class Aoc2019ApplicationDay16 : CommandLineRunner {
 		}
 	}
 
-	fun applyPattern(signal: List<Int>, pattern: List<Int>) : List<Int> {
-		var rvalue = ArrayList<Int>()
+	fun applyPattern(signal: List<Long>, pattern: List<Long>, it : Int) : List<Long> {
+		var rvalue = ArrayList<Long>()
 		for (i in 0 until signal.size) {
-			var entry = 0
-			val patternIterator = getIterator(i, pattern)
+			println("Running $i / ${signal.size}  => $it / 100..")
+			var entry = 0L
+			val patternIterator = getIterator(i.toLong(), pattern)
 			for (j in 0 until signal.size) {
 				val paternEntry = patternIterator.next()
 				entry +=  signal[j] * paternEntry // Check this ..
 			}
-			val digit = entry.toString().last().toString().toInt()
+			val digit = entry.toString().last().toString().toLong()
 			rvalue.add(digit)
 		}
 		return rvalue
@@ -61,12 +61,27 @@ class Aoc2019ApplicationDay16 : CommandLineRunner {
 
 		bufferedReader.useLines {
 			it.forEach { line ->
-				val basePattern = listOf(0, 1, 0, -1)
-				var numbers = line.toCharArray().map { it.toString().toInt() }
+				val basePattern = listOf(0L, 1L, 0L, -1L)
+				var numbers : List<Long>
+				numbers = line.toCharArray().map { it.toString().toLong() }
 				(0 until 100).forEach {
-					numbers = applyPattern(numbers, basePattern)
+					numbers = applyPattern(numbers, basePattern, it)
 				}
 				println("Result: ${numbers.take(8).joinToString(prefix = "", postfix = "", separator = "") }")
+
+				val first7numbers = line.toCharArray()
+						                       .take(7)
+						                       .map { it.toString() }
+						                       .joinToString(prefix = "", postfix = "", separator = "")
+						                       .toInt()
+				numbers = line.repeat(1000).toCharArray().map { it.toString().toLong() }
+				(0 until 100).forEach {
+					numbers = applyPattern(numbers, basePattern, it)
+				}
+				val finalOutput = numbers.drop(first7numbers)
+						                        .take(8)
+						                        .joinToString(prefix = "", postfix = "", separator = "")
+				println("Final output ${first7numbers} => ${finalOutput} ..")
 			}
 		}
 	}
