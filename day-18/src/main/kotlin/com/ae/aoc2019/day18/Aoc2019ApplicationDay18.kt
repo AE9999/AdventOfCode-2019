0b13fa6b5fd2fc3dcbb443397639beb45c518568
ae <ae>
2019-12-18 20:18:59 +0100

    Work as is

--- day-18/src/main/kotlin/com/ae/aoc2019/day18/Aoc2019ApplicationDay18.kt
+++ day-18/src/main/kotlin/com/ae/aoc2019/day18/Aoc2019ApplicationDay18.kt
@@ -7,7 +7,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.RuntimeException
 import java.math.BigInteger
-import java.util.ArrayList
+import java.util.*
 import kotlin.math.absoluteValue
 import kotlin.math.max
 import kotlin.math.min
@@ -24,12 +24,65 @@ class Aoc2019ApplicationDay18 : CommandLineRunner {
 		fun right(): Point {  return Point(x + 1, y  ) }
 	}
 
+	data class SearchEntry(val point: Point,
+						   val distance: Int)
+
+	fun distance(a: Point, b: Point, panel: HashMap<Point, String>) : Int {
+		val visitedPoints = HashSet<Point>()
+		val searchQueue = ArrayDeque<SearchEntry>()
+		searchQueue.push(SearchEntry(a, 0))
+		while (searchQueue.isNotEmpty()) {
+
+			val searchEntry = searchQueue.pop()
+			if (visitedPoints.contains(searchEntry.point)) {
+				continue
+			}
+			visitedPoints.add(searchEntry.point)
+
+			if (searchEntry.point == b) {
+				return searchEntry.distance
+			}
+
+			if (panel.getValue(searchEntry.point) == "#") {
+				continue
+			}
+
+			val upPoint = searchEntry.point.up()
+			if (!visitedPoints.contains(upPoint)) {
+				searchQueue.add(SearchEntry(upPoint, searchEntry.distance + 1))
+			}
+
+			val downPoint = searchEntry.point.down()
+			if (!visitedPoints.contains(downPoint)) {
+				searchQueue.add(SearchEntry(downPoint, searchEntry.distance + 1))
+			}
+
+			val leftPoint = searchEntry.point.left()
+			if (!visitedPoints.contains(leftPoint)) {
+				searchQueue.add(SearchEntry(leftPoint, searchEntry.distance + 1))
+			}
+
+			val rightpoint = searchEntry.point.right()
+			if (!visitedPoints.contains(rightpoint)) {
+				searchQueue.add(SearchEntry(rightpoint, searchEntry.distance + 1))
+			}
+		}
+		throw RuntimeException("Unable to calculate distance ..")
+	}
+
+	private fun solve(currentLocation: Point,
+					  keys: HashMap<String, Point>,
+					  doors: HashMap<String, Point>,
+					  panel: MutableMap<Point, String>): Int {
+
+	}
+
 	override fun run(vararg args: String?) {
 		val bufferedReader = BufferedReader(InputStreamReader(Aoc2019ApplicationDay18::class.java.getResourceAsStream(args[0]!!)))
 		val panel = HashMap<Point, String>().withDefault { "#" }
 		val doors = HashMap<String, Point>()
 		val keys = HashMap<String, Point>()
-		var currentLocation: Point?
+		var currentLocation: Point? = null
 		var currentY = 0
 		var maxX = 0
 
@@ -50,6 +103,9 @@ class Aoc2019ApplicationDay18 : CommandLineRunner {
 				currentY++
 			}
 		}
+		val result = solve(currentLocation!!, keys, doors, panel)
+		println("Need at least $result step to reclaim all keys")
+
 	}
 }
 
